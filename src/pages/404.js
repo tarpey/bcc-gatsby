3b21beb3306cef0cb65b34006b94bd7acaec59d4
James Tarpey <hello@jamestarpey.com>
2020-05-15 11:49:31 +0100

    Added location to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from "react"
 
 import Wrapper from "../components/Wrapper"
 
-export default () => (
-  <Wrapper>
+export default ({ location }) => (
+  <Wrapper location={location}>
     <h1>Page not found</h1>
     <p>The page might have been moved or deleted.</p>
   </Wrapper>
