9940a2fe0c0b321731306c24de01572088ec77c7
James Tarpey <hello@jamestarpey.com>
2020-05-15 11:26:01 +0100

    Updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,9 @@
 import React from "react"
 
 import Wrapper from "../components/Wrapper"
-import SEO from "../components/SEO"
 
 export default () => (
   <Wrapper>
-    <SEO title="404: Not found" />
     <h1>Page not found</h1>
     <p>The page might have been moved or deleted.</p>
   </Wrapper>
